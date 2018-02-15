26f57ea6042eec2702046644dc9c8b8a75ae99db
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2018-02-15 16:11:33 +0100

    v 0.1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name = "libecc100",
-      version = "0.1.3",
+      version = "0.1.4",
       description = "Library for communication with Attocube ECC100 controllers",
       author = "Mikel Eguiraun",
       author_email = "mikel.eguiraun@maxiv.lu.se",
